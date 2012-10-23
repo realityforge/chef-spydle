8f322d3209b82df8e026426faf1a0a98cef0ae0b
Peter Donald <peter@realityforge.org>
2012-10-23 16:50:27 +1100

    Bump the version

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "peter@realityforge.org"
 license          "Apache 2.0"
 description      "Installs/Configures the Spydle Server Monitor"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
 depends "java"
