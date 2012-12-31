daa5c75c398890cc885894f963f226151e311efa
Peter Donald <peter@realityforge.org>
2012-12-31 11:44:26 +1100

    Move to single quotes

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             "spydle"
-maintainer       "Peter Donald"
-maintainer_email "peter@realityforge.org"
-license          "Apache 2.0"
-description      "Installs/Configures the Spydle Server Monitor"
+name             'spydle'
+maintainer       'Peter Donald'
+maintainer_email 'peter@realityforge.org'
+license          'Apache 2.0'
+description      'Installs/Configures the Spydle Server Monitor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          '0.0.3'
 
-depends "java"
+depends 'java'
